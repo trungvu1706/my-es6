bd8a4d8409cfb87ac3f43729ebd582815cadfa32
TrungVu <trungvu1706@gmail.com>
2020-12-30 00:17:37 +0700

    arrow function part 1

--- index.js
+++ index.js
@@ -9,4 +9,10 @@ const mouse = {
 // mouse.sayHi() => function context
 const say = mouse.sayHi.bind(mouse); // gan bien say
 // say = function()
-say(); // khong cos object o dang trc => global context
+// say(); // khong cos object o dang trc => global context
+
+const arr = [1, 2, 3];
+
+const a = arr.map(x => x * x); // arrow function (fat arrow)
+
+console.log(a);
