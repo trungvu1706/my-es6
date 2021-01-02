5b34e61bcecc9fa0967e6b8ac2ea8c3eecefb356
TrungVu <trungvu1706@gmail.com>
2021-01-02 21:11:27 +0700

    super in class

--- index2.js
+++ index2.js
@@ -32,4 +32,44 @@ class Dog extends Animal { // Dog thua ke lai cac method cua animal
 
 const dog = new Dog('Milu');
 // console.log(dog.name);
-dog.eat();
+// dog.eat();
+
+//super 
+class Hero {
+    constructor(name, hp, damage) {
+        this.name = name;
+        this.hp = hp;
+        this.damage = damage;
+    }
+
+    applyDamage(damage) {
+        this.hp -= damage;
+    }
+
+    attack(enermy) {
+        enermy.applyDamage(this.damage);
+    }
+}
+
+
+
+class RangedHero extends Hero { //subClass
+    constructor(name, hp, damage, ranged) {
+        super(name, hp, damage); // super dai dien cho class Hero
+        this.ranged = ranged;
+    }
+
+    attack(enermy) {
+        super.attack(enermy);
+        this.hp += this.damage;
+    }
+}
+
+//before attack
+const heroA = new RangedHero('A', 100, 200, 10);
+const heroB = new Hero('B', 300, 300);
+console.log({ heroA, heroB });
+
+//after attack
+heroA.attack(heroB);
+console.log({ heroA, heroB });
