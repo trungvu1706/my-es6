febe20f87bfaa51584502f477ae06cd388b8b900
TrungVu <trungvu1706@gmail.com>
2021-01-02 16:41:09 +0700

    class inheritance

--- index2.js
+++ index2.js
@@ -1,5 +1,4 @@
 // class in javascript
-
 class Mouse {
     constructor(name) {
         this.name = name;
@@ -11,4 +10,26 @@ class Mouse {
 }
 
 const mouse = new Mouse('Jerry');
-mouse.run();
+// mouse.run();
+
+//claas inheritance
+class Animal {
+    constructor(name) {
+        this.name = name;
+
+    }
+
+    eat() {
+        console.log('eatting');
+    }
+}
+
+class Dog extends Animal { // Dog thua ke lai cac method cua animal
+    bark() {
+        console.log('gau gau...');
+    }
+}
+
+const dog = new Dog('Milu');
+// console.log(dog.name);
+dog.eat();
